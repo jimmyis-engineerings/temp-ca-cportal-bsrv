2227697e7ebbc36fc4fe3c53779c477ae7f6afba
Jimmy Israkhan <jimmy-israkhan@cloud-ace.com>
2025-05-27 01:33:25 +0700

    Temp 3

--- src/configs/sqlite.ts
+++ src/configs/sqlite.ts
@@ -2,17 +2,21 @@
 const ENV = process.env //|| import.meta.env;
 const { SQLITE_DATABASE_PATH } = ENV;
 const databasePath = SQLITE_DATABASE_PATH;
-const shouldCreate = true;
+const shouldCreate = false;
 const shouldReadOnly = false;
-const shouldStrictMode = true;
+const shouldStrictMode = false; // Will automatically create the database if it does not exist
 const shouldSafeIntegers = false;
 
-export default {
-    databasePath,
-    options: {
+const options = (!shouldCreate && !shouldReadOnly && !shouldStrictMode && !shouldSafeIntegers) 
+    ? null 
+    : {
         create: shouldCreate,
         readonly: shouldReadOnly,
         strict: shouldStrictMode,
         safeIntegers: shouldSafeIntegers
     }
+
+export default {
+    databasePath,
+    options
 }
